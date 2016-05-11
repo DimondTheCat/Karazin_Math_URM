f962f9e229f108d4959b58e23041e1e1a4e88514
Dmitry.Kotenko <dmitry.kotenko@appus.me>
2016-05-11 19:39:45 +0300

    start implementing of code editor component

--- src/urm/Controllers/Controller.java
+++ src/urm/Controllers/Controller.java
@@ -1,4 +1,18 @@
 package urm.Controllers;
 
+import javafx.fxml.FXML;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
+
 public class Controller {
+
+    @FXML
+    CodeArea codeArea;
+
+    @FXML
+    public void initialize(){
+
+        codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
+    }
+
 }
